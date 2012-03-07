b40854f5cf5dde69550d7831dc829e49b2efcb71
František Hába <hello@frantisekhaba.com>
2012-03-07 23:11:03 +0100

    Added a new test resource ‘params’

---
+++ tests/resources/params.js
@@ -0,0 +1,11 @@
+module.exports = [
+  '/?foo',
+  '/?foo=bar',
+  '/path/to/file/',
+  '/path/to/file/?query',
+  '/path/to/file/?foo1=bar1&foo2=bar2',
+  '/index.html',
+  '/myPage.html',
+  '/my-Page.html',
+  '/my_Page_hello.htm'
+];
