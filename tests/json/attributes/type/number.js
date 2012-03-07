f58b92f22295326aff0e969b75cb85935a109514
František Hába <hello@frantisekhaba.com>
2012-03-07 20:43:27 +0100

    Rewrite the ‘number’ test into Mocha

---
+++ tests/json/attributes/type/number.js
@@ -0,0 +1,71 @@
+if (typeof module !== 'undefined' && module.exports) {
+  var expect = require('expect.js');
+  var Amanda = require('../../../../dist/latest.js');
+}
+
+/**
+ * Type
+ * --------------------
+ */
+suite('JSON/Attribute/type#number', function() {
+
+  var count = 0;
+
+  /**
+   * Schema
+   */
+  var schema = {
+    type: 'number'
+  };
+
+  /**
+   * Validator
+   */
+  var Validator = new Amanda('json');
+
+  test('should not return an error', function() {
+    [
+      0,
+      676,
+      737,
+      157,
+      617,
+      303,
+      545,
+      183,
+      893,
+      773,
+      253,
+      686,
+      12.12,
+      212.2917,
+      -21,
+      -21.212
+    ].forEach(function(instance) {
+      Validator.validate(instance, schema, function(error) {
+        count += 1;
+        expect(error).to.not.be.ok();
+      });
+    });
+  });
+
+  test('should return an error', function() {
+    [
+      'Hello',
+      true,
+      {},
+      [],
+      function() {}
+    ].forEach(function(instance) {
+      Validator.validate(instance, schema, function(error) {
+        count += 1;
+        expect(error).to.be.ok();
+      });
+    });
+  });
+
+  test('should run 21 times', function() {
+    expect(count).to.be.eql(21);
+  });
+
+});
