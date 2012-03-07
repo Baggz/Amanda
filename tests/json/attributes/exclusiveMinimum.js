2fb9672e99f1259eb92aad648d71fb29a4af5c76
František Hába <hello@frantisekhaba.com>
2012-03-07 20:09:08 +0100

    Rewrite the ‘exclusiveMinimum’ test into Mocha

---
+++ tests/json/attributes/exclusiveMinimum.js
@@ -0,0 +1,72 @@
+if (typeof module !== 'undefined' && module.exports) {
+  var expect = require('expect.js');
+  var Amanda = require('../../../dist/latest.js');
+}
+
+/**
+ * ExclusiveMinimum
+ * --------------------
+ */
+suite('JSON/Attribute/exclusiveMinimum', function() {
+
+  var count = 0;
+
+  /**
+   * Schema
+   */
+  var schema = {
+    type: 'number',
+    minimum: 10,
+    exclusiveMinimum: true
+  };
+
+  /**
+   * Validator
+   */
+  var Validator = new Amanda('json');
+
+  test('should return an error', function() {
+    [
+      0,
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
+      10
+    ].forEach(function(input) {
+      Validator.validate(input, schema, function(error) {
+        count += 1;
+        expect(error).to.be.ok();
+      });
+    });
+  });
+
+  test('should not return an error', function() {
+    [
+      11,
+      12,
+      13,
+      14,
+      15,
+      20,
+      100,
+      1000,
+      10000
+    ].forEach(function(input) {
+      Validator.validate(input, schema, function(error) {
+        count += 1;
+        expect(error).to.not.be.ok();
+      });
+    });
+  });
+
+  test('should run 20 times', function() {
+    expect(count).to.be.eql(20);
+  });
+
+});
