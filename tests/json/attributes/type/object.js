e10f95daf014a5062faca54c461c214a27cb8c11
František Hába <hello@frantisekhaba.com>
2012-03-07 20:43:34 +0100

    Rewrite the ‘object’ test into Mocha

---
+++ tests/json/attributes/type/object.js
@@ -0,0 +1,56 @@
+if (typeof module !== 'undefined' && module.exports) {
+  var expect = require('expect.js');
+  var Amanda = require('../../../../dist/latest.js');
+}
+
+/**
+ * Type
+ * --------------------
+ */
+suite('JSON/Attribute/type#array', function() {
+
+  var count = 0;
+
+  /**
+   * Schema
+   */
+  var schema = {
+    type: 'object'
+  };
+
+  /**
+   * Validator
+   */
+  var Validator = new Amanda('json');
+
+  test('should not return an error', function() {
+    [
+      {}
+    ].forEach(function(instance) {
+      Validator.validate(instance, schema, function(error) {
+        count += 1;
+        expect(error).to.not.be.ok();
+      });
+    });
+  });
+
+  test('should return an error', function() {
+    [
+      'Hello',
+      123,
+      true,
+      [],
+      function() {},
+    ].forEach(function(instance) {
+      Validator.validate(instance, schema, function(error) {
+        count += 1;
+        expect(error).to.be.ok();
+      });
+    });
+  });
+
+  test('should run 6 times', function() {
+    expect(count).to.be.eql(6);
+  });
+
+});
