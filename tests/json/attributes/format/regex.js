57ae3b9e748f4f3dcf7d0f18286b37f335e79940
František Hába <hello@frantisekhaba.com>
2012-03-07 23:10:02 +0100

    Rewrite the ‘regex’ test suite into Mocha

---
+++ tests/json/attributes/format/regex.js
@@ -0,0 +1,58 @@
+if (typeof module !== 'undefined' && module.exports) {
+  var expect = require('expect.js');
+  var Amanda = require('../../../../dist/latest.js');
+}
+
+/**
+ * Format
+ * --------------------
+ */
+suite('JSON/Attribute/format#regex', function() {
+
+  var count = 0;
+
+  /**
+   * Schema
+   */
+  var schema = {
+    format: 'regex'
+  };
+
+  /**
+   * Validator
+   */
+  var Validator = new Amanda('json');
+
+  test('should not return an error', function() {
+    [
+      new RegExp(),
+      /a/gi
+    ].forEach(function(data) {
+      Validator.validate(data, schema, function(error) {
+        count += 1;
+        expect(error).to.not.be.ok();
+      });  
+    });
+  });
+
+  test('should return an error', function() {
+    [
+      new Date(),
+      [],
+      {},
+      function() {},
+      '11/11/11',
+      11
+    ].forEach(function(data) {
+      Validator.validate(data, schema, function(error) {
+        count += 1;
+        expect(error).to.be.ok();
+      });  
+    });
+  });
+
+  test('should run 8 times', function() {
+    expect(count).to.be.eql(8);
+  });
+
+});
