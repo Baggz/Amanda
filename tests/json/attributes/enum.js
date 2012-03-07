c15808ba4698c1f796e4339642f819b1470a5bb1
František Hába <hello@frantisekhaba.com>
2012-03-07 20:08:53 +0100

    Rewrite the ‘enum’ test into Mocha

---
+++ tests/json/attributes/enum.js
@@ -0,0 +1,62 @@
+if (typeof module !== 'undefined' && module.exports) {
+  var expect = require('expect.js');
+  var Amanda = require('../../../dist/latest.js');
+}
+
+/**
+ * Enum
+ * --------------------
+ */
+suite('JSON/Attribute/enum', function() {
+
+  var count = 0;
+
+  /**
+   * Schema
+   */
+  var schema = {
+    type: 'string',
+    enum: [
+      'admin',
+      'administrator'
+    ]
+  };
+
+  /**
+   * Validator
+   */
+  var Validator = new Amanda('json');
+
+  test('should return an error when an instance equals ‘superadmin’', function() {
+    Validator.validate('superadmin', schema, function(error) {
+      count += 1;
+      expect(error).to.be.ok();
+    });
+  });
+
+  test('should return an error when an instance equals ‘amanda’', function() {
+    Validator.validate('amanda', schema, function(error) {
+      count += 1;
+      expect(error).to.be.ok();
+    });
+  });
+
+  test('should not return an error when an instance equals ‘admin’', function() {
+    Validator.validate('admin', schema, function(error) {
+      count += 1;
+      expect(error).to.not.be.ok();
+    });
+  });
+
+  test('should not return an error when an instance equals ‘administrator’', function() {
+    Validator.validate('administrator', schema, function(error) {
+      count += 1;
+      expect(error).to.not.be.ok();
+    });
+  });
+
+  test('should run 4 times', function() {
+    expect(count).to.be.eql(4);
+  });
+
+});
