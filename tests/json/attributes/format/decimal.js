0b565d3d55e7a7e44325d4b62510b44f9b55e9b4
František Hába <hello@frantisekhaba.com>
2012-03-07 21:14:49 +0100

    Rewrite the ‘decimal’ test into Mocha

---
+++ tests/json/attributes/format/decimal.js
@@ -0,0 +1,67 @@
+if (typeof module !== 'undefined' && module.exports) {
+  var expect = require('expect.js');
+  var Amanda = require('../../../../dist/latest.js');
+}
+
+/**
+ * Format
+ * --------------------
+ */
+suite('JSON/Attribute/format#decimal', function() {
+
+  var count = 0;
+
+  /**
+   * Schema
+   */
+  var schema = {
+    format: 'decimal'
+  };
+
+  /**
+   * Validator
+   */
+  var Validator = new Amanda('json');
+
+  test('should not return an error', function() {
+    [
+      1,
+      10,
+      20,
+      30,
+      1.11,
+      1.23,
+      1.30,
+      230.36
+    ].forEach(function(data) {
+      Validator.validate(data, schema, function(error) {
+        count += 1;
+        expect(error).to.not.be.ok();
+      });
+    });
+  });
+
+  test('should return an error', function() {
+    [
+      1.123981273,
+      19723.129319,
+      '+@#$~^*{}',
+      'lorem ipsum',
+      ' ',
+      null,
+      [],
+      {},
+      function() {}
+    ].forEach(function(instance) {
+      Validator.validate(instance, schema, function(error) {
+        count += 1;
+        expect(error).to.be.ok();
+      });
+    });
+  });
+
+  test('should run 17 times', function() {
+    expect(count).to.be.eql(17);
+  });
+
+});
