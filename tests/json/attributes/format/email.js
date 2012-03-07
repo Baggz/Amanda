0943172beb74f72b940141eecc08a42122cf29fe
František Hába <hello@frantisekhaba.com>
2012-03-07 21:14:55 +0100

    Rewrite the ‘email’ test into Mocha

---
+++ tests/json/attributes/format/email.js
@@ -0,0 +1,47 @@
+if (typeof module !== 'undefined' && module.exports) {
+  var expect = require('expect.js');
+  var Amanda = require('../../../../dist/latest.js');
+  var domains = require('../../../resources/domains.js');
+  var emails = require('../../../resources/emails.js');
+}
+
+/**
+ * Format
+ * --------------------
+ */
+suite('JSON/Attribute/format#email', function() {
+
+  var count = 0;
+
+  /**
+   * Schema
+   */
+  var schema = {
+    format: 'email'
+  };
+
+  /**
+   * Validator
+   */
+  var Validator = new Amanda('json');
+
+  test('should not return an error', function() {
+
+    domains.forEach(function(domain) {
+      emails.forEach(function(name) {
+
+        Validator.validate(name + '@' + 'example' + domain, schema, function(error) {
+          count += 1;
+          expect(error).to.not.be.ok();
+        });
+
+      });
+    });
+
+  });
+
+  test('should run 1890 times', function() {
+    expect(count).to.be.eql(1890);
+  });
+
+});
