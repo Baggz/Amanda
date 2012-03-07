d1ef558d3595269e8e10dc112087b4575a29effc
František Hába <hello@frantisekhaba.com>
2012-03-07 20:43:21 +0100

    Rewrite the ‘function’ test into Mocha

---
+++ tests/json/attributes/type/function.js
@@ -0,0 +1,57 @@
+if (typeof module !== 'undefined' && module.exports) {
+  var expect = require('expect.js');
+  var Amanda = require('../../../../dist/latest.js');
+}
+
+/**
+ * Type
+ * --------------------
+ */
+suite('JSON/Attribute/type#function', function() {
+
+  var count = 0;
+
+  /**
+   * Schema
+   */
+  var schema = {
+    type: 'function'
+  };
+
+  /**
+   * Validator
+   */
+  var Validator = new Amanda('json');
+
+  test('should not return an error', function() {
+    [
+      function() {},
+      (function() {})
+    ].forEach(function(instance) {
+      Validator.validate(instance, schema, function(error) {
+        count += 1;
+        expect(error).to.not.be.ok();
+      });
+    });
+  });
+
+  test('should return an error', function() {
+    [
+      'Hello',
+      123,
+      true,
+      {},
+      []
+    ].forEach(function(instance) {
+      Validator.validate(instance, schema, function(error) {
+        count += 1;
+        expect(error).to.be.ok();
+      });
+    });
+  });
+
+  test('should run 7 times', function() {
+    expect(count).to.be.eql(7);
+  });
+
+});
