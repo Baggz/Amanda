388d2487bdff585408ead312be7708c7a51c7d3f
František Hába <hello@frantisekhaba.com>
2012-03-07 21:14:38 +0100

    Rewrite the ‘alphanumeric’ test into Mocha

---
+++ tests/json/attributes/format/alphanumeric.js
@@ -0,0 +1,61 @@
+if (typeof module !== 'undefined' && module.exports) {
+  var expect = require('expect.js');
+  var Amanda = require('../../../../dist/latest.js');
+}
+
+/**
+ * Format
+ * --------------------
+ */
+suite('JSON/Attribute/format#alphanumeric', function() {
+
+  var count = 0;
+
+  /**
+   * Schema
+   */
+  var schema = {
+    format: 'alphanumeric'
+  };
+
+  /**
+   * Validator
+   */
+  var Validator = new Amanda('json');
+
+  test('should not return an error', function() {
+    [
+      'abc123',
+      '123abc',
+      'a1b2c3',
+      'abc',
+      'ABC',
+      'Abc',
+      '123',
+      123
+    ].forEach(function(data) {
+      Validator.validate(data, schema, function(error) {
+        count += 1;
+        expect(error).to.not.be.ok();
+      });  
+    });
+  });
+
+  test('should return an error', function() {
+    [
+      '+@#$~^*{}',
+      'lorem ipsum',
+      ' ',
+    ].forEach(function(data) {
+      Validator.validate(data, schema, function(error) {
+        count += 1;
+        expect(error).to.be.ok();
+      });  
+    });
+  });
+
+  test('should run 11 times', function() {
+    expect(count).to.be.eql(11);
+  });
+
+});
