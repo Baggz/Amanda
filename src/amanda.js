c4889d15662ca1db013c5406cee8efe1a66d7731
František Hába <hello@frantisekhaba.com>
2012-03-07 23:32:28 +0100

    Added a compatibility layer

--- src/amanda.js
+++ src/amanda.js
@@ -1,36 +1,40 @@
 var tmpValidators = {};
 
+var Amanda = function(engine) {
+
+  this.engine = engine;
+  this.validate = Amanda.validate;
+  this.addValidator = Amanda.addValidator;
+
+};
+
 /**
  * Amanda
  *
  * @constructor
  * @param {string} engine
  */
-var Amanda = {
+Amanda.validate = function(instance, schema, options, callback) {
 
-  validate: function(instance, schema, options, callback) {
-
-    if (typeof options === 'function') {
-      callback = options;
-      options = {};
-    }
-
-    each(tmpValidators, engines.json.addValidator);
-
-    return engines.json.validate(instance, schema, options, callback);  
-
-  },
-
-  /**
-   * AddValidator
-   *
-   * @param {string} validatorName
-   * @param {function} validatorFn
-   */
-  addValidator: function(validatorName, validatorFn) {
-    tmpValidators[validatorName] = validatorFn;
+  if (typeof options === 'function') {
+    callback = options;
+    options = {};
   }
 
+  each(tmpValidators, engines.json.addValidator);
 
+  this.engine = this.engine || 'json';
 
+  return engines[this.engine].validate(instance, schema, options, callback);  
+
+};
+
+/**
+ * AddValidator
+ *
+ * @param {string} validatorName
+ * @param {function} validatorFn
+ */
+Amanda.addValidator = function(validatorName, validatorFn) {
+  tmpValidators[validatorName] = validatorFn;
 };
