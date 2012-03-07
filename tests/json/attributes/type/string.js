d1b805e7f698c1981ec06e8ccb9b6422b456119e
František Hába <hello@frantisekhaba.com>
2012-03-07 20:43:41 +0100

    Rewrite the ‘string’ test into Mocha

---
+++ tests/json/attributes/type/string.js
@@ -0,0 +1,57 @@
+if (typeof module !== 'undefined' && module.exports) {
+  var expect = require('expect.js');
+  var Amanda = require('../../../../dist/latest.js');
+}
+
+/**
+ * Type
+ * --------------------
+ */
+suite('JSON/Attribute/type#array', function() {
+
+  var count = 0;
+
+  /**
+   * Schema
+   */
+  var schema = {
+    type: 'string'
+  };
+
+  /**
+   * Validator
+   */
+  var Validator = new Amanda('json');
+
+  test('should not return an error', function() {
+    [
+      'Hello',
+      '123'
+    ].forEach(function(instance) {
+      Validator.validate(instance, schema, function(error) {
+        count += 1;
+        expect(error).to.not.be.ok();
+      });
+    });
+  });
+
+  test('should return an error', function() {
+    [
+      123,
+      true,
+      {},
+      [],
+      function() {},
+    ].forEach(function(instance) {
+      Validator.validate(instance, schema, function(error) {
+        count += 1;
+        expect(error).to.be.ok();
+      });
+    });
+  });
+
+  test('should run 7 times', function() {
+    expect(count).to.be.eql(7);
+  });
+
+});
