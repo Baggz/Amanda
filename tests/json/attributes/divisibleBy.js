54f048b5916fd0854440b5e6f680094973dbe8f3
František Hába <hello@frantisekhaba.com>
2012-03-07 20:08:48 +0100

    Rewrite the ‘divisibleBy’ test into Mocha

---
+++ tests/json/attributes/divisibleBy.js
@@ -0,0 +1,73 @@
+if (typeof module !== 'undefined' && module.exports) {
+  var expect = require('expect.js');
+  var Amanda = require('../../../dist/latest.js');
+}
+
+/**
+ * DivisibleBy
+ * --------------------
+ */
+suite('JSON/Attribute/divisibleBy', function() {
+
+  var count = 0;
+
+  /**
+   * Schema
+   */
+  var schema = {
+    type: 'number',
+    divisibleBy: 2
+  };
+
+  /**
+   * Validator
+   */
+  var Validator = new Amanda('json');
+
+  test('should return an error', function() {
+    [
+      1,
+      3,
+      5,
+      7,
+      9,
+      11,
+      111,
+      1111
+    ].forEach(function(input) {
+      Validator.validate(input, schema, function(error) {
+        count += 1;
+        expect(error).to.be.ok();
+      });
+    });
+  });
+
+  test('should not return an error', function() {
+    [
+      2,
+      4,
+      6,
+      8,
+      10,
+      12,
+      14,
+      16,
+      18,
+      20,
+      22,
+      40,
+      100,
+      1000
+    ].forEach(function(input) {
+      Validator.validate(input, schema, function(error) {
+        count += 1;
+        expect(error).to.not.be.ok();
+      });
+    });
+  });
+
+  test('should run 22 times', function() {
+    expect(count).to.be.eql(22);
+  });
+
+});
