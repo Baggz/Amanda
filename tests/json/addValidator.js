4366cce2ac39aa3a3840bf609cd72e0d222e1931
František Hába <hello@frantisekhaba.com>
2012-03-07 23:27:27 +0100

    Rewrite the ‘addValidator’ test suite into Mocha

---
+++ tests/json/addValidator.js
@@ -0,0 +1,100 @@
+if (typeof module !== 'undefined' && module.exports) {
+  var expect = require('expect.js');
+  var Amanda = require('../../dist/latest.js');
+}
+
+/**
+ * AddValidator
+ * --------------------
+ */
+suite('JSON/addValidator', function() {
+
+  var count = 0;
+
+  /**
+   * Validator
+   */
+  var Validator = new Amanda('json');
+
+  /**
+   * MyValidator
+   */
+  Validator.addValidator('myValidator', function(property, propertyValue, validator, propertyValidators, callback) {
+
+    var usernames = [
+      'Baggz',
+      'Amanda',
+      'Administrator',
+      'Admin'
+    ];
+
+    if (validator && usernames.indexOf(propertyValue) !== -1) {
+      return callback('Oops! This username - ' + propertyValue + ' - is taken.');
+    } else {
+      return callback();
+    }
+
+  });
+
+  /**
+   * Schema
+   */
+  var schema = {
+    reqired: true,
+    myValidator: true
+  };
+
+  /**
+   * Schema2
+   */
+  var schema2 = {
+    type: 'object',
+    properties: {
+      name: {
+        required: true,
+        type: 'string',
+        format: 'alphanumeric',
+        myValidator: true
+      },
+      username: {
+        required: true,
+        type: 'string',
+        format: 'alphanumeric',
+      }
+    }
+  };
+
+  test('should return an error', function() {
+    [
+      'Baggz',
+      'Amanda',
+      'Administrator',
+      'Admin'
+    ].forEach(function(instance) {
+      Validator.validate(instance, schema, function(error) {
+        expect(error).to.be.ok();
+      });
+    });
+  });
+
+  test('should return an error', function() {
+    [
+      'Baggz',
+      'Amanda',
+      'Administrator',
+      'Admin'
+    ].forEach(function(input) {
+
+      Validator.validate({
+        name: input,
+        username: 'Test'
+      }, schema2, {
+        singleError: false
+      }, function(error) {
+        expect(error).to.be.ok();
+      });
+
+    });
+  });
+
+});
