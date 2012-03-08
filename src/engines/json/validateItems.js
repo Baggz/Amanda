f965e3fd7db19cd9a6cf2088c4b9445c27d75e2a
František Hába <hello@frantisekhaba.com>
2012-03-08 19:47:48 +0100

    Move the ‘Validation.prototype.validateItems’ method into a separate file

---
+++ src/engines/json/validateItems.js
@@ -0,0 +1,73 @@
+/**
+ * Validation.validateItems
+ *
+ * @param {object} instance
+ * @param {object} schema
+ * @param {string} path
+ * @param {function} callback
+ */
+Validation.prototype.validateItems = function(instance, schema, path, callback) {
+
+  // Save a reference to the ‘this’
+  var self = this;
+
+  // If the instance is not empty
+  if (instance && !isEmpty(instance)) {
+
+    /**
+     * {
+     *   type: 'array',
+     *   items: {
+     *     type: 'object'
+     *   }
+     * }
+     * — or —
+     * {
+     *   type: 'array',
+     *   items: {
+     *     type: 'array'
+     *   }
+     * }
+     */
+    if (['object', 'array'].indexOf(schema.items.type) !== -1) {
+      return each(instance, function(index, propertyValue, callback) {
+
+        var propertyPath = path + '[' + index + ']';
+
+        return self.validateSchema(
+          propertyValue,
+          schema.items,
+          propertyPath,
+          callback
+        );
+
+      }, callback);
+
+    /*
+     * {
+     *   type: 'array',
+     *   items: {
+     *     type: 'string'
+     *   }
+     * }
+     */
+    } else {
+      return each(instance, function(index, propertyValue, callback) {
+
+        var propertyPath = path + '[' + index + ']';
+
+        return self.validateProperty(
+          propertyPath,
+          propertyValue,
+          schema.items,
+          callback
+        );
+
+      }, callback);
+    }
+
+  } else {
+    return callback();
+  }
+
+};
