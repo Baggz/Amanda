b335e0f23844a64a6fe88aa3524ee55e231a0400
František Hába <hello@frantisekhaba.com>
2012-03-09 22:32:52 +0100

    Add a new method ‘addAttributeConstructor’

---
+++ src/engines/json/addAttributeConstructor.js
@@ -0,0 +1,9 @@
+/**
+ * AddAttributeConstructor
+ *
+ * @param {string} attributeName
+ * @param {function} attributeConstructor
+ */
+Validation.prototype.addAttributeConstructor = function(attributeName, attributeConstructor) {
+  return Validation.prototype.attributes[attributeName] = attributeConstructor.call(this);
+};
