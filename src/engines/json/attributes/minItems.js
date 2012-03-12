e10b82e4f0ed1b9f6e384faa9825111760348fdc
František Hába <hello@frantisekhaba.com>
2012-03-12 15:49:55 +0100

    Change the way the ‘minItems’ attribute reports errors

--- src/engines/json/attributes/minItems.js
+++ src/engines/json/attributes/minItems.js
@@ -2,13 +2,13 @@
  * MinItems
  */
 Validation.prototype.addAttributeConstructor('minItems', function minItemsConstructor() {
-
   return function minItems(property, propertyValue, attributeValue, propertyAttributes, callback) {
-    if (isArray(propertyValue) && propertyValue.length >= attributeValue) {
-      return callback();
-    } else {
-      return callback(true);
-    }
-  };
 
+    if (isArray(propertyValue) && propertyValue.length < attributeValue) {
+      this.addError();
+    }
+
+    return callback();
+
+  };
 });
