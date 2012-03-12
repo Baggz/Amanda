1fc9fb25a9e8e0e5747070100abf93a2237b6773
František Hába <hello@frantisekhaba.com>
2012-03-12 15:49:08 +0100

    Change the way the ‘length’ attribute reports errors

--- src/engines/json/attributes/length.js
+++ src/engines/json/attributes/length.js
@@ -2,13 +2,13 @@
  * Length
  */
 Validation.prototype.addAttributeConstructor('length', function lengthConstructor() {
-
   return function length(property, propertyValue, attributeValue, propertyAttributes, callback) {
-    if (isString(propertyValue) && propertyValue.length === attributeValue) {
-      return callback();
-    } else {
-      return callback(true);
-    }
-  };
 
+    if (isString(propertyValue) && propertyValue.length !== attributeValue) {
+      this.addError();
+    }
+
+    return callback();
+
+  };
 });
