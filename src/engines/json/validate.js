62c719f4976d40979c3918fcfad48616fcd25c15
František Hába <hello@frantisekhaba.com>
2012-03-12 16:04:31 +0100

    The ‘validate’ method now accepts JSON

--- src/engines/json/validate.js
+++ src/engines/json/validate.js
@@ -11,8 +11,91 @@ Validation.prototype.validate = function(instance, schema, callback) {
   // Save a reference to the ‘this’
   var self = this;
 
-  return this.validateSchema(instance, schema, '', function(error) {
-    return callback((self.Errors.length > 0) ? self.Errors : undefined);
-  });
+  this.instance = instance;
+  this.schema = schema;
+
+  /**
+   * Basic Types
+   */
+  var basicTypes = [
+    'string',
+    'number',
+    'function',
+    'boolean',
+    'integer',
+    'int',
+    'null'
+  ];
+
+  /**
+   * Object Types
+   */
+  var objectTypes = [
+    'object', 
+    'array'
+  ];
+
+  /**
+   * CallbackProxy
+   */
+  var callbackProxy = function() {
+    if (self.errors.length !== 0) {
+      return callback(self.errors);
+    } else {
+      return callback();
+    }
+  };
+
+  /**
+   * {
+   *   type: 'string',
+   *   ...
+   * }
+   */
+  if (basicTypes.indexOf(schema.type) !== -1) {
+    return this.validateProperty(undefined, instance, schema, callbackProxy);
+  }
+
+  /**
+   * {
+   *   type: 'object',
+   *   ...
+   * }
+   */
+  if (objectTypes.indexOf(schema.type) !== -1) {
+    if (isString(instance)) {
+      try {
+        instance = JSON.parse(instance);
+      } catch(parseError) {
+        throw new Error(parseError);
+      }
+    }
+    return this.validateSchema(instance, schema, '', callbackProxy);
+  }
+
+  /**
+   * {
+   *   type: ???,
+   *   ...
+   * }
+   */
+  if (schema.type === 'any' || !schema.type) {
+
+    if (isString(instance)) {
+      try {
+        instance = JSON.parse(instance);
+        return this.validateSchema(instance, schema, '', callbackProxy);
+      } catch(parseError2) {
+
+      }
+    }
+
+    if (isObject(instance) || isArray (instance)) {
+      return this.validateSchema(instance, schema, '', callbackProxy); 
+    }
+
+    return this.validateProperty(undefined, instance, schema, callbackProxy);
+
+  }
 
 };
