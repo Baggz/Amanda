c4938cec885a34b0f639c978b45d7685dfe7772e
František Hába <hello@frantisekhaba.com>
2012-03-12 15:49:44 +0100

    Change the way the ‘maxLength’ attribute reports errors

--- src/engines/json/attributes/maxLength.js
+++ src/engines/json/attributes/maxLength.js
@@ -2,13 +2,13 @@
  * MaxLength
  */
 Validation.prototype.addAttributeConstructor('maxLength', function maxLengthConstructor() {
-
   return function maxLength(property, propertyValue, attributeValue, propertyAttributes, callback) {
-    if (isString(propertyValue) && propertyValue.length <= attributeValue) {
-      return callback();
-    } else {
-      return callback(true);
-    }
-  };
 
+    if (isString(propertyValue) && propertyValue.length > attributeValue) {
+      this.addError();
+    }
+    
+    return callback();
+
+  };
 });
