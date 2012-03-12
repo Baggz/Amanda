70e4ac7780372ab0082346b839fa7532a64cc731
František Hába <hello@frantisekhaba.com>
2012-03-12 15:49:39 +0100

    Change the way the ‘maxItems’ attribute reports errors

--- src/engines/json/attributes/maxItems.js
+++ src/engines/json/attributes/maxItems.js
@@ -2,13 +2,13 @@
  * MaxItems
  */
 Validation.prototype.addAttributeConstructor('maxItems', function maxItemsConstructor() {
-
   return function maxItems(property, propertyValue, attributeValue, propertyAttributes, callback) {
-    if (isArray(propertyValue) && propertyValue.length <= attributeValue) {
-      return callback();
-    } else {
-      return callback(true);
-    }
-  };
 
+    if (isArray(propertyValue) && propertyValue.length > attributeValue) {
+      this.addError();
+    }
+
+    return callback();
+
+  };
 });
