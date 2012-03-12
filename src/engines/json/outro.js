fc62b2d419b66425c680bed18bb0cbefe285b606
František Hába <hello@frantisekhaba.com>
2012-03-12 19:48:43 +0100

    Minor improvements to the export of the JSON engine

--- src/engines/json/outro.js
+++ src/engines/json/outro.js
@@ -1,46 +1,56 @@
-
+  
   /**
    * Export
    * --------------------
    */
-  engines.json = {
+  engines.json = (function() {
 
     /**
-     * Validate
-     *
-     * @param {object} instance
-     * @param {object} schema
-     * @param {object} options
-     * @param {function} callback
+     * Cache
      */
-    validate: function(instance, schema, options, callback) {
-      if (typeof options === 'function') {
-        callback = options;
-        options = {};
+    var cache = [];
+    var cacheIndex = {};
+
+    return {
+
+      /**
+       * Validate
+       *
+       * @param {object} instance
+       * @param {object} schema
+       * @param {object} options
+       * @param {function} callback
+       */
+      validate: function(instance, schema, options, callback) {
+        if (typeof options === 'function') {
+          callback = options;
+          options = {};
+        }
+        return (new Validation(options)).validate(instance, schema, callback);
+      },
+
+      /**
+       * AddAttribute
+       *
+       * @param {string} attributeName
+       * @param {function} attributeFn
+       */
+      addAttribute: function(attributeName, attributeFn) {
+        return Validation.prototype.addAttribute.apply(Validation, argument);
+      },
+
+      /**
+       * AddAttributeConstructor
+       *
+       * @param {string} attributeName
+       * @param {function} attributeConstructor
+       */
+      addAttributeConstructor: function(attributeName, attributeConstructor) {
+        return Validation.prototype.addAttributeConstructor.apply(Validation, argument);
       }
-      return (new Validation(options)).validate(instance, schema, callback);
-    },
 
-    /**
-     * AddAttribute
-     *
-     * @param {string} attributeName
-     * @param {function} attributeFn
-     */
-    addAttribute: function(attributeName, attributeFn) {
-      return Validation.prototype.addAttribute.apply(Validation, argument);
-    },
+    };
 
-    /**
-     * AddAttributeConstructor
-     *Constructor
-     * @param {string} attributeName
-     * @param {function} attributeConstructor
-     */
-    addAttributeConstructor: function(attributeName, attributeConstructor) {
-      return Validation.prototype.addAttributeConstructor.apply(Validation, argument);
-    },
-
-  };
+  }());
 
 }());
