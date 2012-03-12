2423b2d3371bda697687708f124548e4f3f20cc4
František Hába <hello@frantisekhaba.com>
2012-03-12 15:49:32 +0100

    Change the way the ‘maximum’ attribute reports errors

--- src/engines/json/attributes/maximum.js
+++ src/engines/json/attributes/maximum.js
@@ -2,14 +2,15 @@
  * Maximum
  */
 Validation.prototype.addAttributeConstructor('required', function maximumConstructor() {
-
   return function maximum(property, propertyValue, attributeValue, propertyAttributes, callback) {
-    if (isNumber(propertyValue)) {
-      var condition = (propertyAttributes.exclusiveMaximum) ? propertyValue < attributeValue : propertyValue <= attributeValue;
-      return (condition) ? callback() : callback(true);
-    } else {
-      return callback(true);
-    }
-  };
 
+    if (isNumber(propertyValue)) {
+      if ((propertyAttributes.exclusiveMaximum && propertyValue >= attributeValue) || (propertyValue > attributeValue)) {
+        this.addError();
+      }
+    }
+  
+    return callback();
+
+  };
 });
