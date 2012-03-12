47f7feae081db8939c35fb10f9a8689d1fb30ada
František Hába <hello@frantisekhaba.com>
2012-03-12 15:51:31 +0100

    Move the ‘additionalProperties’ attribute into a separate file

---
+++ src/engines/json/attributes/additionalProperties.js
@@ -0,0 +1,10 @@
+/**
+ * AdditionalProperties
+ */
+Validation.prototype.addAttributeConstructor('additionalProperties', function additionalPropertiesConstructor() {
+  return function additionalProperties(property, propertyValue, attributeValue, propertyAttributes, callback) {
+    
+    return callback();
+  
+  };
+});
