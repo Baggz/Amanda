cf2eb9736b6ad8d71c7d9aa6be003c254450096c
František Hába <hello@frantisekhaba.com>
2012-03-12 15:59:26 +0100

    Expose only certain methods from the validation process

--- src/engines/json/validateProperty.js
+++ src/engines/json/validateProperty.js
@@ -1,7 +1,7 @@
 /**
  * Validation.validateProperty
  *
- * @param {string} property
+ * @param {string} propertyName
  * @param {object} propertyAttributes
  * @param {string|object} propertyValue
  * @param {boolean} singleError
@@ -12,52 +12,59 @@ Validation.prototype.validateProperty = function(property, propertyValue, proper
   // Save a reference to the ‘this’
   var self = this;
 
+  var context = {};
+
+  [
+    'validateItems',
+    'validateProperties',
+    'validateSchema',
+    'validateProperty',
+    'getProperty'
+  ].forEach(function(key) {
+    context[key] = this[key];
+  }, self);
+
   /**
    * Iterator
    *
    * @param {string} attributeName
+   * @param {function} attributeFn
    * @param {function} callback
    */
   var iterator = function(attributeName, attributeFn, callback) {
 
+    var lastLength = self.errors.length;
+
+    // Overwrite the ‘addError’ method
+    context.addError = function(message) {
+      return self.errors.push({
+        property: property,
+        propertyValue: propertyValue,
+        attributeName: attributeName,
+        attributeValue: propertyAttributes[attributeName],
+        message: message
+      });
+    };
+
     /**
      * OnComplete
-     *
-     * @param {object} error
      */
-    var onComplete = function(error) {
-
-      if (!error) return callback();
-
-      // Renders an error messaage
-      var errorMessage = self.renderErrorMessage(attributeName, {
-        property: property,
-        propertyValue: propertyValue,
-        attribute: propertyAttributes[attributeName]
-      });
-
-      // Add a new error
-      self.Errors.addError({
-        property: property,
-        propertyValue: propertyValue,
-        attribute: attributeName,
-        attributeValue: propertyAttributes[attributeName],
-        message: errorMessage
-      });
-
-      // If the ‘singleError’ is on, stop the validation process
-      return callback(self.singleError ? true : null);
-
+    var onComplete = function() {
+      if (self.errors.length > lastLength && self.singleError) {
+        return callback(true);
+      } else {
+        return callback();
+      }
     };
 
     if (propertyAttributes[attributeName]) {
-      return attributeFn(
+      return attributeFn.apply(context, [
         property,
         propertyValue,
         propertyAttributes[attributeName],
         propertyAttributes,
         onComplete
-      );
+      ]);
     } else {
       return callback();
     }
@@ -65,10 +72,11 @@ Validation.prototype.validateProperty = function(property, propertyValue, proper
   };
 
   // If it's not a required param and it's empty, skip
-  if (propertyAttributes.required !== true && typeof propertyValue === 'undefined') {
+  if (propertyAttributes.required !== true && isUndefined(propertyValue)) {
     return callback();
-  } else {
-    return each(self.attributes, iterator, callback);
   }
+  
+  // Validate the property  
+  return each(self.attributes, iterator, callback);
 
 };
