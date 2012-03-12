e7c05ce9c1dc91a95f481140acbed17f7297f535
František Hába <hello@frantisekhaba.com>
2012-03-12 15:50:13 +0100

    Change the way the ‘required’ attribute reports errors

--- src/engines/json/attributes/required.js
+++ src/engines/json/attributes/required.js
@@ -1,14 +1,15 @@
 /**
  * Required
+ * --------------------
  */
 Validation.prototype.addAttributeConstructor('required', function requiredConstructor() {
-
   return function required(property, propertyValue, attributeValue, propertyAttributes, callback) {
-    if (attributeValue && isUndefined(propertyValue)) {
-      return callback(true);
-    } else {
-      return callback();
-    }
-  };
 
+    if (attributeValue && isUndefined(propertyValue)) {
+      this.addError();
+    }
+
+    return callback();
+
+  };
 });
