f608c9b7158dc3082aee10da7b158216744ddcfa
František Hába <hello@frantisekhaba.com>
2012-03-12 19:54:53 +0100

    The build script now includes all files

--- tools/compile.js
+++ tools/compile.js
@@ -1,24 +1,77 @@
+// Load dependencies
 var async = require('async');
 var fs = require('fs');
 
 console.log('\nCompiling...');
 
+
+
 var files = [
   './src/intro.js',
+  
+  // Utils
+  './src/utils/detectType.js',
   './src/utils/each.js',
+  './src/utils/every.js',
+  './src/utils/hasProperty.js',
   './src/utils/isArray.js',
+  './src/utils/isBoolean.js',
   './src/utils/isEmpty.js',
+  './src/utils/isFunction.js',
+  './src/utils/isInteger.js',
+  './src/utils/isNull.js',
+  './src/utils/isNumber.js',
   './src/utils/isObject.js',
+  './src/utils/isString.js',
+  './src/utils/isUndefined.js',
+  './src/utils/keys.js',
+  //'./src/utils/md5.js',
   './src/utils/merge.js',
   './src/utils/pluck.js',
+  './src/utils/returnTrue.js',
+  './src/utils/some.js',
+
+
   './src/engines/json/intro.js',
-  './src/engines/json/messages.js',
-  './src/engines/json/error.js',
-  './src/engines/json/attributes.js',
   './src/engines/json/validation.js',
+
+  './src/engines/json/attributes.js',
+  './src/engines/json/addAttribute.js',
+  './src/engines/json/addAttributeConstructor.js',
+
+  './src/engines/json/attributes/additionalItems.js',
+  './src/engines/json/attributes/additionalProperties.js',
+  './src/engines/json/attributes/divisibleBy.js',
+  './src/engines/json/attributes/enum.js',
+  './src/engines/json/attributes/except.js',
+  './src/engines/json/attributes/format.js',
+  './src/engines/json/attributes/length.js',
+  './src/engines/json/attributes/maximum.js',
+  './src/engines/json/attributes/maxItems.js',
+  './src/engines/json/attributes/maxLength.js',
+  './src/engines/json/attributes/minimum.js',
+  './src/engines/json/attributes/minItems.js',
+  './src/engines/json/attributes/minLength.js',
+  './src/engines/json/attributes/pattern.js',
+  './src/engines/json/attributes/required.js',
+  './src/engines/json/attributes/type.js',
+  './src/engines/json/attributes/uniqueItems.js',
+
+  './src/engines/json/validationError.js',
+  './src/engines/json/errorMessages.js',
+  './src/engines/json/getProperty.js',
+
+  './src/engines/json/validate.js',
+  './src/engines/json/validateItems.js',
+  './src/engines/json/validateProperties.js',
+  './src/engines/json/validateProperty.js',
+  './src/engines/json/validateSchema.js',
+
   './src/engines/json/outro.js',
+
   './src/amanda.js',
   './src/outro.js'
+
 ];
 
 var handleError = function(error) {
