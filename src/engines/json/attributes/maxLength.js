dd0ecb50849de448342ec05bf04c5d1412b45e3a
František Hába <hello@frantisekhaba.com>
2012-03-14 18:23:07 +0100

    Improve the readability of the ‘maxLength’ attribute

--- src/engines/json/attributes/maxLength.js
+++ src/engines/json/attributes/maxLength.js
@@ -2,10 +2,13 @@
  * MaxLength
  */
 var maxLengthAttribute = function maxLength(property, propertyValue, attributeValue, propertyAttributes, callback) {
+
   if (isString(propertyValue) && propertyValue.length > attributeValue) {
     this.addError();
   }
+
   return callback();
+
 };
 
 // Export
