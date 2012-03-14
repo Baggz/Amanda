58da343d6a803a9c8b6435a696d91455905cc6bf
František Hába <hello@frantisekhaba.com>
2012-03-14 19:54:21 +0100

    Add a compatibility layer

--- src/amanda.js
+++ src/amanda.js
@@ -7,3 +7,43 @@ var Amanda = function(engine) {
   return engines[engine];
 
 };
+
+/**
+ * Amanda.validate
+ *
+ * This method is deprecated, please use ‘amanda('json').validate’ instead.
+ */
+Amanda.validate = function(instance, schema, options, callback) {
+  var json = engines.json;
+  return json.validate.apply(json, arguments);
+};
+
+/**
+ * Amanda.addValidator
+ *
+ * This method is deprecated, please use ‘amanda('json').addValidator’ instead.
+ */
+Amanda.addValidator = function(attributeName, attributeFn) {
+  var json = engines.json;
+  return json.addAttribute.apply(json, arguments);
+};
+
+/**
+ * Amanda.addAttribute
+ *
+ * This method is deprecated, please use ‘amanda('json').addAttribute’ instead.
+ */
+Amanda.addAttribute = function(attributeName, attributeFn) {
+  var json = engines.json;
+  return json.addAttribute.apply(json, arguments);
+};
+
+/**
+ * Amanda.addAttributeConstructor
+ *
+ * This method is deprecated, please use ‘amanda('json').addAttributeConstructor’ instead.
+ */
+Amanda.addAttributeConstructor = function(attributeName, attributeConstructor) {
+  var json = engines.json;
+  return json.addAttributeConstructor.apply(json, arguments);
+};
