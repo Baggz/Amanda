95ebd82c280f8ce5d72d823177a3168b76400280
František Hába <hello@frantisekhaba.com>
2012-03-14 18:22:45 +0100

    The ‘divisibleBy’ attribute is now fully compliant with the JSON Schema Internet Draft

--- src/engines/json/attributes/divisibleBy.js
+++ src/engines/json/attributes/divisibleBy.js
@@ -2,10 +2,17 @@
  * DivisibleBy
  */
 var divisibleByAttribute = function divisibleBy(property, propertyValue, attributeValue, propertyAttributes, callback) {
+
+  if (attributeValue === 0) {
+    throw new Error('The value of this attribute should not be 0.');
+  }
+
   if (isNumber(propertyValue) && (propertyValue % attributeValue !== 0)) {
     this.addError();
   }
+
   return callback();
+
 };
 
 // Export
