340d0ab8f107b96b3a92654b1c01065339a1acc7
František Hába <hello@frantisekhaba.com>
2012-03-15 15:50:21 +0100

    Implement the ‘additionalProperties’ attribute

--- src/engines/json/validateItems.js
+++ src/engines/json/validateItems.js
@@ -11,61 +11,81 @@ Validation.prototype.validateItems = function(instance, schema, path, callback) 
   // Save a reference to the ‘this’
   var self = this;
 
-  // If the instance is not empty
-  if (instance && !isEmpty(instance)) {
+  /**
+   * {
+   *   type: 'array'
+   *   items: [
+   *     {
+   *       type: 'string'
+   *     },
+   *     {
+   *       type: 'number'
+   *     },
+   *     ...
+   *   ],
+   *   ...
+   * }
+   */
+  if (isArray(schema.items)) {
 
-    /**
-     * {
-     *   type: 'array',
-     *   items: {
-     *     type: 'object'
-     *   }
-     * }
-     * — or —
-     * {
-     *   type: 'array',
-     *   items: {
-     *     type: 'array'
-     *   }
-     * }
-     */
-    if (['object', 'array'].indexOf(schema.items.type) !== -1) {
-      return each(instance, function(index, propertyValue, callback) {
-
-        var propertyPath = self.joinPath(path, index);
-
+    // Additional items are allowed
+    if (isUndefined(schema.additionalItems) || schema.additionalItems === true) {
+      return each(schema.items, function(itemIndex, itemSchema, callback) {
         return self.validateSchema(
-          propertyValue,
-          schema.items,
-          propertyPath,
+          instance[itemIndex],
+          itemSchema,
+          self.joinPath(path, itemIndex),
           callback
         );
-
-      }, callback);
-
-    /*
-     * {
-     *   type: 'array',
-     *   items: {
-     *     type: 'string'
-     *   }
-     * }
-     */
-    } else {
-      return each(instance, function(index, propertyValue, callback) {
-
-        var propertyPath = self.joinPath(path, index);
-
-        return self.validateProperty(
-          propertyPath,
-          propertyValue,
-          schema.items,
-          callback
-        );
-
       }, callback);
     }
 
+    return each(instance, function(itemIndex, itemValue, callback) {
+
+      // The ‘additionalItems’ attribute is a schema that defines
+      // the schema of the additional items
+      if (schema.items[itemIndex] || isObject(schema.additionalItems)) {
+        return self.validateSchema(
+          itemValue,
+          schema.items[itemIndex],
+          self.joinPath(path, itemIndex),
+          callback
+        );
+      }
+
+      // Additional items are disallowed
+      if (schema.additionalItems === false) {
+        self.errors.push({
+          property: self.joinPath(path, itemIndex),
+          propertyValue: itemValue,
+          attributeName: 'additionalItems',
+          attributeValue: false
+        });
+        return callback();
+      }
+
+    }, callback);
+
+  }
+
+  /**
+   * {
+   *   type: 'array'
+   *   items: {
+   *     type: 'string'
+   *   },
+   *   ...
+   * }
+   */
+  if (isObject(schema.items) && instance && !isEmpty(instance)) {
+    return each(instance, function(itemIndex, itemValue, callback) {
+      return self.validateSchema(
+        instance[itemIndex],
+        schema.items,
+        self.joinPath(path, itemIndex),
+        callback
+      );
+    }, callback);
   } else {
     return callback();
   }
