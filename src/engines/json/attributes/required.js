0462a5658369c3095503e0321a510edef8a41578
František Hába <hello@frantisekhaba.com>
2012-03-15 21:54:05 +0100

    Minor improvements to the ‘required’ attribute

--- src/engines/json/attributes/required.js
+++ src/engines/json/attributes/required.js
@@ -3,7 +3,7 @@
  */
 var requiredAttribute = function required(property, propertyValue, attributeValue, propertyAttributes, callback) {
 
-  if (attributeValue && isUndefined(propertyValue)) {
+  if (attributeValue && (isUndefined(propertyValue) || isEmpty(propertyValue))) {
     this.addError();
   }
 
