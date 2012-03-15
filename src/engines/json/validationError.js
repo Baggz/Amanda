3fcd9793869d53ee151ebae4099ad610371360bf
František Hába <hello@frantisekhaba.com>
2012-03-15 15:54:51 +0100

    Implement the ‘getProperties’ method

--- src/engines/json/validationError.js
+++ src/engines/json/validationError.js
@@ -11,3 +11,14 @@ ValidationError.prototype.push = function(item) {
   this[this.length] = item;
   this.length += 1;
 };
+
+/**
+ * GetProperties
+ */
+ValidationError.prototype.getProperties = function() {
+  return pluck(this, 'property');
+};
+
+ValidationError.prototype.getMessages = function() {
+
+};
