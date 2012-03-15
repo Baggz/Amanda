030f13dfe9466a711cb932b22914895b11bdb929
František Hába <hello@frantisekhaba.com>
2012-03-15 10:07:42 +0100

    Add tests for backward compatibility

---
+++ tests/json/compatibility.js
@@ -0,0 +1,66 @@
+if (typeof module !== 'undefined' && module.exports) {
+  var expect = require('expect.js');
+  var amanda = require('../../dist/latest.js');
+}
+
+/**
+ * Enum
+ * --------------------
+ */
+suite('JSON/Backward Compatibility', function() {
+
+  var count = 0;
+
+  /**
+   * Schema
+   */
+  var schema = {
+    type: 'string'
+  };
+
+  /**
+   * Validator
+   */
+  var jsonSchemaValidator = amanda('json');
+
+  test('The ‘Error’ object has the ‘getProperties’ method', function() {
+    jsonSchemaValidator.validate(123, schema, function(error) {
+      expect(error).to.be.ok();
+      expect(error).to.have.property('getProperties');
+    });
+  });
+
+  test('The ‘Error’ object has the ‘getMessages’ method', function() {
+    jsonSchemaValidator.validate(123, schema, function(error) {
+      expect(error).to.be.ok();
+      expect(error).to.have.property('getMessages');
+    });
+  });
+
+  test('The ‘Error’ object has all properties', function() {
+    jsonSchemaValidator.validate(123, schema, function(error) {
+      expect(error).to.be.ok();
+      expect(error[0]).to.have.property('property', undefined);
+      expect(error[0]).to.have.property('propertyValue');
+      expect(error[0]).to.have.property('validatorName');
+      expect(error[0]).to.have.property('validatorValue');
+    });
+  });
+
+  test('Amanda has the ‘addValidator’ method', function() {
+    expect(amanda).to.have.property('addValidator');
+  });
+
+  test('Amanda has the ‘getValidators’ method', function() {
+    expect(amanda).to.have.property('getValidators');
+  });
+
+  test('Amanda has the ‘getVersion’ method', function() {
+    expect(amanda).to.have.property('getVersion');
+  });
+
+  test('Amanda has the ‘validate’ method', function() {
+    expect(amanda).to.have.property('validate');
+  });
+
+});
