16e51ffa82c5fb244fd2102a0a73c3d56e41903c
Tully <tully@apiary.io>
2013-06-25 16:53:05 +0200

    property as array

--- releases/latest/amanda.js
+++ releases/latest/amanda.js
@@ -1460,19 +1460,14 @@ Validation.prototype.getProperty = function(property, source) {
  */
 Validation.prototype.joinPath = function(path, property) {
 
-  // If the ‘path’ is undefined (object), convert the path to a string
-  path = path || '';
+  path = path || [];
 
   // Converts the ‘property’ to a string
   property = property + '';
 
-  if (property.match(/^[a-zA-Z][a-zA-Z0-9]*$/)) {
-    return (path) ? (path + '.' + property) : property;
-  } else if (property.match(/\d+/)) {
-    return path + '[' + property + ']';
-  } else  {
-    return path + '["' + property + '"]';
-  }
+  path.push(property);
+
+  return path;
 
 };
 
--- src/engines/json/joinPath.js
+++ src/engines/json/joinPath.js
@@ -6,18 +6,13 @@
  */
 Validation.prototype.joinPath = function(path, property) {
 
-  // If the ‘path’ is undefined (object), convert the path to a string
-  path = path || '';
+  path = path || [];
 
   // Converts the ‘property’ to a string
   property = property + '';
 
-  if (property.match(/^[a-zA-Z_][a-zA-Z0-9_]*$/)) {
-    return (path) ? (path + '.' + property) : property;
-  } else if (property.match(/^\d+$/)) {
-    return path + '[' + property + ']';
-  } else  {
-    return path + '["' + property + '"]';
-  }
+  path.push(property);
+
+  return path;
 
 };
